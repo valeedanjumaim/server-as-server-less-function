89a890efaea6f9af835c8cff07591f665ecdcade
Valeed Anjum <valeed@aimnovo.com>
2025-07-04 12:07:39 +0500

    A Slow End Point

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
 })
 
+app.get('/slow', async (req, res) => {
+  // Simulate a slow operation taking more than 10ms
+  await new Promise(resolve => setTimeout(resolve, 2000)); // 2 second delay
+  res.send('This response was delayed by 2 seconds 🐌');
+})
+
+
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 })
